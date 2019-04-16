4217252f7a8edf49d356fcca83470595a2d1cc9b
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-16 04:31:30 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,15 +2,23 @@ class Owner
   # code goes here
   
   @@all = []
+  @@count = 0 
   
   def initialize(name)
     @name = name
     @@all << self
+    @@count = self += 1 
+    
   end 
   
   def self.all
     @@all 
   end 
+  
+  def self.count 
+    @@count 
+  end 
+  
   
   
   
